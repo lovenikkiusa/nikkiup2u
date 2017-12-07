4a0c81656f2ee01fb5c3fa4ddf0660d022600c9f
lovenikkiusa <34090647+lovenikkiusa@users.noreply.github.com>
2017-12-07 18:00:27 +0800

    Add files via upload

--- data/notice.js
+++ data/notice.js
@@ -4,4 +4,4 @@ var clothesHistoryNotice = "";
 
 //历史更新
 var levelNotice = "";
-var levelHistoryNotice = "<br>历史更新记录。<br>2017.11.25配装器网页修改V1.0。";
+var levelHistoryNotice = "<br>历史更新记录。<br>2017.11.25配装器网页修改V1.0。<br>2017.12.04配装器网页细节修改V1.0.3。<br>2017.12.05更新格莱斯，婚纱，十二章V1.1.0。<br>2017.12.06更新铁羽神迹，远野童子的中文名称V1.1.1。<br>2017.12.07更新时光礼赞套装属性V1.1.2。";
